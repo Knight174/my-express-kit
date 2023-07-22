4e0fcaffdf62e5c704e05dc1527e579170e9f6f2
pinchao <cpc4253@163.com>
2023-07-22 22:22:57 +0800

    响应头

--- index.js
+++ index.js
@@ -88,6 +88,20 @@ app.get("/api/v1/works/search", (request, response) => {
   });
 });
 
+// 响应头（设置响应头参数、设置状态码）
+app.get("/api/v1/workflows", (request, response) => {
+  response.set("X-Eric", "node"); // 设置响应头
+  response.append("X-Eric", "node2"); // 追加响应头
+
+  // response.append("X-Eric2", "node2"); // 追加新的响应头
+
+  response.status(401); // 设置状态码
+
+  response.json({
+    data: response.get("X-Eric"), // 获取请求头，如果是多个则返回一个数组
+  });
+});
+
 const server = app.listen(3000, () => {
   console.log("Server running at http://localhost:" + server.address().port);
 });
