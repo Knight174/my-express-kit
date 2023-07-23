722147b17d6bee334457760589face648f5dd91e
pinchao <cpc4253@163.com>
2023-07-24 00:09:34 +0800

    代码整理，注释添加

--- src/app.ts
+++ src/app.ts
@@ -7,37 +7,36 @@ import requestLogger from './middleware/request-logger';
 // import errorLogger from './middleware/other-logger';
 // errorLogger.error('abcdefg'); // 触发一个错误日志记录
 
-import fn1 from './fn1'; // 外部中间件
+// 自封装中间件
+import logLocals from './middleware/log-locals';
 import testRouter from './routes/api/v1/test';
 import uploadRouter from './routes/api/v1/upload';
 
+// 创建主应用实例
 const app: Application = express();
 // 定义 app 变量，通过任意中间件的 res.app.locals.title 来获取
 app.locals.title = 'this is a title';
 
-// 数据实体大小限制
-app.use(bodyParser.json({ limit: '150kb' }));
-app.use(bodyParser.urlencoded({ limit: '150kb', extended: true }));
+app.use(bodyParser.json({ limit: '150kb' })); // json 数据实体大小限制
+app.use(bodyParser.urlencoded({ limit: '150kb', extended: true })); // 二进制数据实体大小限制
 app.use(express.json()); // 解析请求体中的 json 数据变成对象
 app.use(express.urlencoded({ extended: false })); // 解析 url 编码数据（表单数据）
 app.use(express.static('./public')); // 指定静态目录，先去这里找，找不到就 next()
 app.use(cors()); // 处理 cors 跨域
-app.use(consoleLogger('dev')); // 控制台日志中间件
+app.use(consoleLogger('dev')); // 控制台中打印请求日志
+app.use(requestLogger); // 请求日志记录到日志文件
 
-// 中间件
-// 本地中间件：打印 request 请求体数据
+// 本地中间件：任意请求，打印 request 请求体数据
 app.use((req, res, next) => {
   console.log('req', req.body);
   next();
 });
 // 外部中间件
-app.use(fn1);
-// 导入路由中间件
+app.use(logLocals);
+
+// 路由中间件
 app.use('/api/v1/test', testRouter);
 app.use('/api/v1/upload', uploadRouter);
-
-app.use(requestLogger); // 请求日志中间件
-console.info('requestLogger', requestLogger);
 
 // 路由
 app.get('/', (request, response) => {
--- src/middleware/log-locals.ts
+++ src/middleware/log-locals.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 
-const fn1 = (req: Request, res: Response, next: NextFunction) => {
+const logLocals = (req: Request, res: Response, next: NextFunction) => {
   const title = res.app.locals.title;
   console.log('app.locals.title => ', title);
   next();
 };
 
-export default fn1;
+export default logLocals;
