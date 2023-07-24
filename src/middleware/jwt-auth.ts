44680910920aa23ff798f72f9f15538541a84ae8
pinchao <cpc4253@163.com>
2023-07-24 16:14:58 +0800

    修改不需要 token 校验的 api

--- src/middleware/jwt-auth.ts
+++ src/middleware/jwt-auth.ts
@@ -5,5 +5,5 @@ export default jwt({
   secret: JWT_SECRET,
   algorithms: ['HS256'], // 加密算法
 }).unless({
-  path: ['/api/v2/login', '/api/v2/register'], // 不需要校验 token 的 api
+  path: ['/api/v2/auth/login', '/api/v2/auth/register', /^\/public\/.*/], // 不需要校验 token 的 api
 });
