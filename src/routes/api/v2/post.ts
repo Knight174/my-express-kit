30e7e161b1481ed42a04382af0ad2bbb98064466
pinchao <cpc4253@163.com>
2023-07-26 13:49:10 +0800

    api/v2/posts 接口优化

--- src/routes/api/v2/post.ts
+++ src/routes/api/v2/post.ts
@@ -1,94 +1,105 @@
 import Router from 'express';
 import prisma from '../../../config/prisma';
-import {
-  sendSuccessResponse,
-  sendErrorResponse,
-  handlePrismaError,
-} from '../../../utils/response-util';
+import { sendSuccessResponse } from '../../../utils/response-util';
 
 const router = Router();
 
 // 新增 post
-router.post('/posts', async (req, res) => {
+router.post('/posts', async (req, res, next) => {
   const { title, content, authorId, likesCount, viewsCount } = req.body;
-  const newPost = await prisma.post.create({
-    data: {
-      title,
-      content,
-      likesCount,
-      viewsCount,
-      author: { connect: { id: authorId } },
-    },
-  });
-  if (newPost) {
+  try {
+    await prisma.post.create({
+      data: {
+        title,
+        content,
+        likesCount,
+        viewsCount,
+        author: { connect: { id: authorId } },
+      },
+    });
     sendSuccessResponse(res, '创建成功');
+  } catch (err) {
+    next(err);
   }
 });
 
 // 更新 post 发布状态
-router.put('/publish/:id', async (req, res) => {
+router.put('/publish/:id', async (req, res, next) => {
   const { id } = req.params;
-  const post = await prisma.post.update({
-    where: { id },
-    data: { published: true },
-  });
-  if (post) {
+  try {
+    await prisma.post.update({
+      where: { id },
+      data: { published: true },
+    });
     sendSuccessResponse(res, '状态修改成功');
+  } catch (err) {
+    next(err);
   }
 });
 
 // 删除 post
-router.delete(`/posts/:id`, async (req, res) => {
+router.delete(`/posts/:id`, async (req, res, next) => {
   const { id } = req.params;
-  const post = await prisma.post.delete({
-    where: {
-      id,
-    },
-  });
-  res.json(post);
-  if (post) {
+  try {
+    await prisma.post.delete({
+      where: {
+        id,
+      },
+    });
     sendSuccessResponse(res, '删除成功');
+  } catch (err) {
+    next(err);
   }
 });
 
 // 获取指定 post
-router.get(`/posts/:id`, async (req, res) => {
+router.get(`/posts/:id`, async (req, res, next) => {
   const { id } = req.params;
-  const post = await prisma.post.findUnique({
-    where: {
-      id,
-    },
-  });
-  if (post) {
+  try {
+    const post = await prisma.post.findUnique({
+      where: {
+        id,
+      },
+    });
     sendSuccessResponse(res, '获取成功', post);
+  } catch (err) {
+    next(err);
   }
 });
 
 // 获取所有 post
-router.get('/posts', async (req, res) => {
-  const posts = await prisma.post.findMany({
-    where: { published: true },
-  });
-  sendSuccessResponse(res, '获取成功', posts);
+router.get('/posts', async (req, res, next) => {
+  try {
+    const posts = await prisma.post.findMany({
+      where: { published: true },
+    });
+    sendSuccessResponse(res, '获取成功', posts);
+  } catch (err) {
+    next(err);
+  }
 });
 
 // 获取筛选的 post（模糊查询）
-router.get('/posts', async (req, res) => {
+router.get('/posts', async (req, res, next) => {
   const { title, content } = req.query;
-  const draftPosts = await prisma.post.findMany({
-    where: {
-      // OR：或，满足数组中的某一项
-      OR: [
-        {
-          title: { contains: String(title) },
-        },
-        {
-          content: { contains: String(content) },
-        },
-      ],
-    },
-  });
-  sendSuccessResponse(res, '获取成功', draftPosts);
+  try {
+    const draftPosts = await prisma.post.findMany({
+      where: {
+        // OR：或，满足数组中的某一项
+        OR: [
+          {
+            title: { contains: String(title) },
+          },
+          {
+            content: { contains: String(content) },
+          },
+        ],
+      },
+    });
+    sendSuccessResponse(res, '获取成功', draftPosts);
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
